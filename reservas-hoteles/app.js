e1d2716ee8dbbbe5580d27fdf1016f2897ccd8fa
romy32 <147573797+romy32@users.noreply.github.com>
2026-03-08 21:36:04 +0000

    Actualizar archivos dentro de reservas-hoteles con la versión funcional

--- reservas-hoteles/app.js
+++ reservas-hoteles/app.js
@@ -1,136 +1,30 @@
-// =============================
-// LISTA DE HOTELES
-// =============================
-
 const hoteles = [
+
 {
 nombre:"Hotel Caribe",
 ciudad:"Cartagena",
 precio:120
 },
+
 {
 nombre:"Hotel Playa",
 ciudad:"Santa Marta",
 precio:90
 },
+
 {
 nombre:"Hotel Montaña",
 ciudad:"Medellín",
 precio:100
 }
+
 ]
 
 const container = document.getElementById("hotelContainer")
 
-// =============================
-// MOSTRAR HOTELES
-// =============================
+function mostrarHoteles(){
 
-async function mostrarHoteles(){
-
-let html = ""
-
-for (const hotel of hoteles){
-
-const clima = await obtenerClima(hotel.ciudad)
-
-html += `
-<div class="col-md-4">
-<div class="card mb-4 shadow">
-
-<div class="card-body">
-
-<h5 class="card-title">${hotel.nombre}</h5>
-
-<p>Ciudad: ${hotel.ciudad}</p>
-
-<p>Precio: $${hotel.precio}</p>
-
-<p>Clima actual: ${clima}</p>
-
-<button class="btn btn-primary">
-Reservar
-</button>
-
-</div>
-
-</div>
-</div>
-`
-}
-
-container.innerHTML = html
-
-}
-
-// =============================
-// API CLIMA
-// =============================
-
-async function obtenerClima(ciudad){
-
-try{
-
-const respuesta = await fetch(`https://wttr.in/${ciudad}?format=%t`)
-
-const clima = await respuesta.text()
-
-return clima
-
-}catch(error){
-
-return "No disponible"
-
-}
-
-}
-
-mostrarHoteles()
-
-// =============================
-// VALIDACION FORMULARIO
-// =============================
-
-document.getElementById("formReserva").addEventListener("submit",function(e){
-
-e.preventDefault()
-
-const nombre = document.getElementById("nombre").value.trim()
-const email = document.getElementById("email").value.trim()
-
-if(nombre === "" || email === ""){
-
-alert("Complete todos los campos")
-return
-
-}
-
-if(!email.includes("@")){
-
-alert("Ingrese un correo válido")
-return
-
-}
-
-alert("Reserva realizada con éxito")
-
-})
-// =============================
-// INTEGRANTE 5
-// BUSCADOR
-// =============================
-
-document.getElementById("buscador").addEventListener("input",function(e){
-
-const texto = e.target.value.toLowerCase()
-
-container.innerHTML=""
-
-const filtrados = hoteles.filter(hotel=>
-hotel.nombre.toLowerCase().includes(texto)
-)
-
-filtrados.forEach(hotel=>{
+hoteles.forEach(hotel=>{
 
 container.innerHTML += `
 
@@ -146,6 +40,10 @@ container.innerHTML += `
 
 <p>Precio: $${hotel.precio}</p>
 
+<button class="btn btn-primary">
+Reservar
+</button>
+
 </div>
 
 </div>
@@ -156,4 +54,6 @@ container.innerHTML += `
 
 })
 
-})
+}
+
+mostrarHoteles()
