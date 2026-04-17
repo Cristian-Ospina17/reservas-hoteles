9b51e723ea259f4403fd722a8697af46425aad26
brianstevv <brianstevenao@gmail.com>
2026-04-17 21:56:39 +0000

    feat: sistema de opiniones

--- reservas-hoteles/reviews.js
+++ reservas-hoteles/reviews.js
@@ -1,35 +1,16 @@
+// GUARDAR OPINIÓN
 export function guardarOpinion(hotelId, comentario, rating) {
-  const opiniones = JSON.parse(localStorage.getItem("opiniones")) || [];
-
-  opiniones.push({
+  const nuevaOpinion = OpinionFactory.crearOpinion(
     hotelId,
     comentario,
-    rating: Number(rating)
-  });
+    rating
+  );
 
-  localStorage.setItem("opiniones", JSON.stringify(opiniones));
+  db.opiniones.push(nuevaOpinion);
+  db.guardarOpiniones();
 }
 
+// OBTENER OPINIONES
 export function obtenerOpiniones(hotelId) {
-  const opiniones = JSON.parse(localStorage.getItem("opiniones")) || [];
-  return opiniones.filter(o => o.hotelId == hotelId);
-}
-
-export function promedioHotel(hotelId) {
-  const opiniones = obtenerOpiniones(hotelId);
-  if (opiniones.length === 0) return 0;
-
-  const total = opiniones.reduce((sum, o) => sum + o.rating, 0);
-  return (total / opiniones.length).toFixed(1);
-}
-
-export function topHoteles(hoteles) {
-  return hoteles
-    .map((h, i) => ({
-      ...h,
-      id: i,
-      rating: promedioHotel(i)
-    }))
-    .sort((a, b) => b.rating - a.rating)
-    .slice(0, 3);
+  return db.opiniones.filter(o => o.hotelId == hotelId);
 }
